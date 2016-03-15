92afa3329fd8cc3e56701be3db9fbfed71bc2368
Sander Looise <sanderlooise@gmail.com>
2016-03-15 11:37:22 +0100

    Opdracht 26

--- src/nl/slooise/oefenen/IODemo.java
+++ src/nl/slooise/oefenen/IODemo.java
@@ -6,27 +6,35 @@ public class IODemo {
 	
 	public static void main(String[] args) {
 
+		int getallen [] = new int[10];
+		int getal = 0;
+		boolean vol = false;
 		
-		int teller = 1;
-		int primes = 0;
+		Scanner s = new Scanner (System.in);
 		
-		boolean isPrime = true;
-		
-		while (primes < 10)
+		while (vol == false)
 		{
-			for (int i = 2; i < 1000; i++)
-				if (teller % i != 0)
-					if (teller == i)
-						isPrime = false;
-				
-			if (isPrime = true)
-			{
-				System.out.println(teller);
-				primes++;
-			}
-			teller++;
+			boolean origineel = true;
 			
+			System.out.println("Voer een origineel getal in:");
+			int invoer = Integer.valueOf(s.next());
+			for (int i = 0; i < getallen.length; i++)
+				if (invoer == getallen[i])
+					origineel = false;
+			
+			if (origineel == true)
+			{
+				getallen[getal] = invoer;
+				getal++;
+			}	
+			else
+				System.out.println("Deze heb je al!");
+			
+			if (getal == 10)
+				vol = true;
 		}
 		
+		for (int i = 0; i < getallen.length; i++)
+			System.out.println(getallen[i]);
 	}
 }
