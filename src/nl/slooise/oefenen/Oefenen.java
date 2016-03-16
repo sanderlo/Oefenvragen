b59d87416f0b22fca43c32279c4ea3e362fcec54
Sander Looise <sanderlooise@gmail.com>
2016-03-16 16:35:03 +0100

    Oefenopdracht woensdag

--- src/nl/slooise/oefenen/Oefenen.java
+++ src/nl/slooise/oefenen/Oefenen.java
@@ -1,31 +1,73 @@
 package nl.slooise.oefenen;
 
-public class Oefenen
-{
-	public static void main (String [] args)
-	{
-	
-	int border = 2_000_000;
-	int teller = 1;
-	int sum = 0;
-	
-	boolean isPrime = true;
-	
-	while (teller < border)
-	{
-		for (int i = 2; i < teller; i++)
-			if (teller % i == 0)
-				if (teller != i)
-					isPrime = false;
-	
-		if (isPrime == true)
-		{
-			sum += teller;
-			System.out.println(teller);
-		}
-		teller++;
-		isPrime = true;		
+public class Oefenen {
+
+	public static void main (String [] args){
+		byte a = 2;
+		System.out.println(pastIn(a, true));
 	}
-	System.out.println(sum);
+	
+	public static int aantalbytes(byte argument) {
+		return 1;
+	}
+	
+	public static int aantalbytes(int argument) {
+		return 4;
+	}
+	
+	public static int aantalbytes(double argument) {
+		return 8;
+	}
+	
+	public static int aantalbytes(float argument) {
+		return 4;
+	}
+	
+	public static int aantalbytes(char argument) {
+		return 2;
+	}
+	
+	public static int aantalbytes(boolean argument) {
+		return 4;
+	}
+	
+	public static int aantalbytes(long argument) {
+		return 8;
+	}
+	
+	public static int aantalbytes(short argument) {
+		return 2;
+	}
+	
+	public static boolean pastIn(int a, float b){
+		boolean p = false;
+		if (aantalbytes(a) <= aantalbytes(b)){
+			p = true;
+		}
+		return p;
+	}
+	
+	public static boolean pastIn(long a, short b){
+		boolean p = false;
+		if (aantalbytes(a) <= aantalbytes(b)){
+			p = true;
+		}
+		return p;
+	}
+	
+	public static boolean pastIn(byte a, boolean b){
+		boolean p = false;
+		if (aantalbytes(a) <= aantalbytes(b)){
+			p = true;
+		}
+		return p;
+	}
+	
+	public static boolean pastIn(char a, int b){
+		boolean p = false;
+		if (aantalbytes(a) <= aantalbytes(b)){
+			p = true;
+		}
+		return p;
 	}
 }
--- src/nl/slooise/oefenen/Singleton.java
+++ src/nl/slooise/oefenen/Singleton.java
@@ -13,4 +13,17 @@ public class Singleton {
 			new Singleton();
 		return theInstance;
 	}
+	
+	static public String test (int a, short b){
+		return "x";
+	}
+	
+	static public String test (int a, byte b){
+		return "X";
+	}
+	
+	public static void main (String [] args){
+		byte a = 3;
+		System.out.println(test(3,a));
+	}
 }
